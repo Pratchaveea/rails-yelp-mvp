713f97febb0169663f08a8e65af64cce58fd0845
Pratchavee Amornsetthachai <pratchavee.amorn@gmail.com>
2024-08-07 23:17:08 +0900

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Fill the DB with some instances we can use
+
+puts "Cleaning the DB..."
+Restaurant.destroy_all
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Creating #{CATEGORIES.count} Restaurants..."
+CATEGORIES.shuffle.each do |category|
+  Restaurant.create!(
+    name: Faker::Restaurant.unique.name,
+    address: Faker::Address.street_address,
+    category: category,
+  )
+end
+puts "... created #{Restaurant.count} restaurants"
